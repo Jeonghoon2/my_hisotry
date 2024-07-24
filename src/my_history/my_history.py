6295cd453020a79cfbf406ee78cdc8998c61664c
DE32 <DE32@jeongui-MacBookPro.local>
2024-07-24 18:17:49 +0900

    가변 인자 값으로 개수 또는 날짜 지정으로 출력

--- src/my_history/my_history.py
+++ src/my_history/my_history.py
@@ -1,9 +1,34 @@
 import pandas as pd
-import sys
+import argparse
+
+
+# -s : count
+# -t : 지정 횟수로 해당 일자의 커맨드 값 조회
+
+def init():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-s', required=False)
+    parser.add_argument('-d', required=False)
+    parser.add_argument('-t', required=False)
+    return parser
+
 
 def cnt():
-    df = pd.read_parquet("~/Workspace/playdata/tmp/history.parquet")
-    fdf = df[df['cmd'].str.contains(sys.argv[1])]
-    cnt = fdf['cnt'].sum()
-    print(f"{sys.argv[1]} 커맨드 사용 횟수는 {cnt}회 입니다.")
+    # Parquet 파일 읽기
+    df = pd.read_parquet(path="~/data/parquet")
 
+    # 인자값 초기화
+    parser = init()
+    args = parser.parse_args().__dict__
+
+    # s값이 있다면
+    if args['s'] is not None:
+        fdf = df[df['cmd'] == args['s']]
+        cnt = fdf['cnt'].sum()
+        print(f"{args['s']} 커맨드 사용 횟수는 {cnt}회 입니다.")
+    elif args['t'] is not None:
+        fdf = df[df['dt'] == args['d']]
+
+        fdf = fdf.head(int(args.get('t')))
+        print(fdf.to_string(index=False))
---
+++ src/my_history/my_history2.py
@@ -0,0 +1,10 @@
+class Action():
+	def __init__(self):
+		pass
+
+	def cnt(self, **kwargs):
+		pass
+
+if __name__ == "__main__":
+	print()
+
